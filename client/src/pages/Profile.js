3255b01151ad72b0c9e29cc60d70954b6e4d2174
Kris Pennimpede <kpennimpede@hotmail.com>
2021-04-29 16:25:19 -0400

    add friend function working and thought form mocked up but not connected to server yet

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useParams, Redirect } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-import { useQuery } from '@apollo/react-hooks';
+import ThoughtForm from '../components/ThoughtForm';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 
@@ -14,9 +16,9 @@ const Profile = () => {
     variables: { username: userParam }
   });
 
-  console.log(data);
-
   const user = data?.me || data?.user || {};
+
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   //redirect to personal profile page if usernaem is logged in user's
   if (Auth.loggedIn() && Auth.getprofile().data.username === userParam) {
@@ -35,12 +37,28 @@ const Profile = () => {
     )
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile. 
         </h2>
+
+        {userParam && (
+          <button className='btn ml-auto' onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -56,6 +74,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className='mb-3'>{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
